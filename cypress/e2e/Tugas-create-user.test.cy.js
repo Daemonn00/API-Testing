c9b7594c8eb2275927af03c5638f1b903b8ccae3
Deden Septiawan <122546569+Daemonn00@users.noreply.github.com>
2023-04-03 13:41:43 +0700

    Update Tugas-create-user.test.cy.js

--- cypress/e2e/Tugas-create-user.test.cy.js
+++ cypress/e2e/Tugas-create-user.test.cy.js
@@ -7,8 +7,7 @@ describe("Create New User",()=>{
         }
         cy.request('POST','https://reqres.in/api/users', user).then((response)=>{
             expect(response.status).equal(201)
-            expect(response.body.name).to.e
-            q(user.name)
+            expect(response.body.name).to.eq(user.name)
             expect(response.body.job).to.eq(user.job)
         }).as('users')
         cy.get('@users').its('body').should('contain',user)
